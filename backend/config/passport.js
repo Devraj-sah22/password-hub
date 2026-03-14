63fef6039a2782e297270459f0bb0ee5c8cc72a4
Devraj-sah22 <devrajsah9813@gmail.com>
2026-03-15 00:13:18 +0530

    Fixed Continue with Github

--- backend/config/passport.js
+++ backend/config/passport.js
@@ -99,7 +99,8 @@ module.exports = function (passport) {
   passport.use(new GithubStrategy({
     clientID: process.env.GITHUB_CLIENT_ID,
     clientSecret: process.env.GITHUB_CLIENT_SECRET,
-    callbackURL: '/api/auth/github/callback'
+    //callbackURL: '/api/auth/github/callback'
+    callbackURL: "https://password-hub-o450.onrender.com/api/auth/github/callback"
   }, async (accessToken, refreshToken, profile, done) => {
     try {
       let user = await User.findOne({ githubId: profile.id });
