e78a69dd67c5a4244a3cd496c2ac4cf47ed504ea
Devraj-sah22 <devrajsah9813@gmail.com>
2026-03-14 17:18:50 +0530

    fix: smtp

--- backend/server.js
+++ backend/server.js
@@ -25,8 +25,7 @@ app.use(cors({
   //origin: 'http://localhost:3000',
   origin: [
     "http://localhost:3000",
-    "https://password-hub-five.vercel.app",
-    "https://password-hub-five.vercel.app/"
+    "https://password-hub-five.vercel.app",,
   ],
   credentials: true
 }));
