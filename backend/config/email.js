55008bd8ce6df49ad180ffcb9a7abd8f908a59e1
Devraj-sah22 <devrajsah9813@gmail.com>
2026-03-14 14:23:13 +0530

    fixed Error

--- backend/config/email.js
+++ backend/config/email.js
@@ -1,10 +1,27 @@
 const nodemailer = require("nodemailer");
 
+// const transporter = nodemailer.createTransport({
+//   service: "gmail",
+//   auth: {
+//     user: process.env.EMAIL_USER,
+//     pass: process.env.EMAIL_PASS
+//   }
+// });
+
+// module.exports = transporter;
+
+const nodemailer = require("nodemailer");
+
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false, // TLS
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS
+  },
+  tls: {
+    rejectUnauthorized: false
   }
 });
 
